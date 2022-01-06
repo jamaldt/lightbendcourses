74f283d5c53ed6fc7ce3a8ed0fbd032acddc8849
jamal <carldeltoro@gmail.com>
2022-01-06 14:13:40 -0400

    Dependy Injection

--- Lightbend-Lagom-Java-Hello-World/exercises/hello-world-impl/src/test/java/com/lightbend/helloworld/HelloWorldServiceImplTest.java
+++ Lightbend-Lagom-Java-Hello-World/exercises/hello-world-impl/src/test/java/com/lightbend/helloworld/HelloWorldServiceImplTest.java
@@ -5,17 +5,24 @@ import org.junit.Test;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
+import static  com.lightbend.lagom.javadsl.testkit.ServiceTest.*;
 
 public class HelloWorldServiceImplTest {
     @Test
     public void helloWorld_ShouldReturnTheExpectedResult() throws Exception{
-        HelloWorldService helloWorldService = new HelloWorldServiceImpl();
-        String result = helloWorldService
-            .helloWorld()
-            .invoke()
-            .toCompletableFuture()
-            .get(5, TimeUnit.SECONDS);
+        withServer(defaultSetup(),server -> {
+            HelloWorldService helloWorldService = server.client(HelloWorldService.class);
 
-        assertEquals("Hello World",result);
+            String result = helloWorldService
+                .helloWorld()
+                .invoke()
+                .toCompletableFuture()
+                .get(5, TimeUnit.SECONDS);
+
+            assertEquals("Hello World",result);
+        });
+
+
+
     }
 }
